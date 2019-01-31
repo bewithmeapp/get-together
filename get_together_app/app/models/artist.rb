75818ebed6cb3a1cf0c8e97cca74c4066d26f3f7
MayMo7a <maymo7a@gmail.com>
2019-01-31 09:53:09 +0300

    auth for artist and model

--- get_together_app/app/models/artist.rb
+++ get_together_app/app/models/artist.rb
@@ -1,2 +1,3 @@
 class Artist < ApplicationRecord
+    has_secure_password
 end
--- get_together_app/db/schema.rb
+++ get_together_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_123050) do
+ActiveRecord::Schema.define(version: 2019_01_30_183434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_123050) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "manikins", force: :cascade do |t|
@@ -50,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_123050) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
